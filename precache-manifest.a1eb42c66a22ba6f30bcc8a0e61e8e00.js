445938146a7952f7032aa8cee6f0e964bd87ab69
parsa sabbar <parsasabbar@gmail.com>
2020-09-17 14:10:34 +0430

    Updates

---
+++ precache-manifest.a1eb42c66a22ba6f30bcc8a0e61e8e00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c708638b85c6c371e67c7f648343081a",
+    "url": "/myprofile/index.html"
+  },
+  {
+    "revision": "ec49e40008da16b2a064",
+    "url": "/myprofile/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f96279a69132181714a",
+    "url": "/myprofile/static/js/2.fa08da9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myprofile/static/js/2.fa08da9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec49e40008da16b2a064",
+    "url": "/myprofile/static/js/main.dd90f8eb.chunk.js"
+  },
+  {
+    "revision": "f2794764b622333aefc7",
+    "url": "/myprofile/static/js/runtime-main.d506aaf2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myprofile/static/media/logo.5d5d9eef.svg"
+  }
+]);
